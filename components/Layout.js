dd22c4d98c97a25906da889f400cf60baa61e49e
Emma Egstad <emma@egstad.com>
2022-05-19 09:27:40 -0500

    added footer nav links and styles

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
+
 import styles from '../styles/layout.module.css';
 
 export default function Layout({ children, activeNav, setActiveNav }) {
@@ -7,6 +9,7 @@ export default function Layout({ children, activeNav, setActiveNav }) {
         <div className={styles.Layout}>
             <Header activeNav={activeNav} setActiveNav={setActiveNav} />
             {children}
+            <Footer />
         </div>
     );
 }
