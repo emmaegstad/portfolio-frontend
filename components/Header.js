fadb957019d4330e5328429282570d8cf419bb75
Emma Egstad <emma@egstad.com>
2022-05-26 14:56:16 -0500

    another header refactor... getting there. made final adjustments" "

--- components/Header.js
+++ components/Header.js
@@ -24,28 +24,32 @@ export default function Header() {
     ];
 
     return (
-        <ul className={styles.header}>
-            {links.map((link) => (
-                <li className={styles.headerLink} key={uuid()}>
-                    <Link href={link.href}>
-                        <a
-                            className={cn({
-                                [utilStyles.button]: true,
-                                [styles[link.name]]: true,
-                                [styles['active']]:
-                                    router.pathname === link.href,
-                            })}
-                        >
-                            {link.name}
-                        </a>
-                    </Link>
-                </li>
-            ))}
-            <li className={`${styles.headerLink} ${styles.last}`}>
-                <Link href="">
-                    <a className={`${utilStyles.button}`}>?</a>
-                </Link>
-            </li>
-        </ul>
+        <div className={styles.header}>
+            <ul className={styles.headerList}>
+                {links.map((link) => (
+                    <li className={styles.headerListItem} key={uuid()}>
+                        <Link href={link.href}>
+                            <a
+                                className={cn({
+                                    [utilStyles.button]: true,
+                                    [styles[link.name]]: true,
+                                    [styles.headerLink]: true,
+                                    [styles['active']]:
+                                        router.pathname === link.href,
+                                })}
+                            >
+                                {link.name}
+                            </a>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+
+            <button
+                className={`${styles.headerLink} ${styles.last} ${utilStyles.button} `}
+            >
+                ?
+            </button>
+        </div>
     );
 }
