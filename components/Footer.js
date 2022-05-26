6b1a3ba627713450cecff11dcb30fabfe69a0e69
Emma Egstad <emma@egstad.com>
2022-05-26 14:02:52 -0500

    adding footer text and creating mobile styles

--- components/Footer.js
+++ components/Footer.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import Link from 'next/link';
 import styles from '../styles/footer.module.css';
+import utilStyles from '../styles/utils.module.css';
 
 export default function Footer() {
     return (
         <div className={styles.footer}>
-            <ul className={styles.footerNav}>
+            <ul className={styles.footerLinks}>
                 <Link href="https://github.com/emmaegstad">
-                    <a className={`${styles.footerLink} ${styles.github}`}>
+                    <a className={`${utilStyles.button} ${styles.footerLink}`}>
                         github
                     </a>
                 </Link>
                 <Link href="https://www.linkedin.com/in/emmaegstad/">
-                    <a className={`${styles.footerLink} ${styles.linkedin}`}>
+                    <a className={`${utilStyles.button} ${styles.footerLink}`}>
                         linkedin
                     </a>
                 </Link>
                 <Link href="mailto:emma@egstad.com">
-                    <a className={`${styles.footerLink} ${styles.email}`}>
+                    <a className={`${utilStyles.button} ${styles.footerLink}`}>
                         email
                     </a>
                 </Link>
             </ul>
+            <section className={styles.footerText}>
+                <span>thanks for scrolling!</span>
+                <span>(c) copyright 2022</span>
+            </section>
         </div>
     );
 }
