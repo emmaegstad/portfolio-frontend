0232d6282b9033a85f46dd5d520a3bc21f5fb3a5
Emma Egstad <emma@egstad.com>
2022-05-31 14:21:18 -0500

    conditionally rendering footer to appear on home and work, not about

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Header from './Header';
 import Footer from './Footer';
 
 import styles from '../styles/layout.module.css';
 
 export default function Layout({ children }) {
+    const [activeFooter, setActiveFooter] = useState(true);
+    const router = useRouter();
+
+    useEffect(() => {
+        if (router.pathname === '/about') {
+            setActiveFooter(false);
+        } else {
+            setActiveFooter(true);
+        }
+    }, [router.pathname]);
+
     return (
         <div className={styles.layout}>
             <Header />
             <main>{children}</main>
-            <Footer />
+            {activeFooter && <Footer />}
         </div>
     );
 }
