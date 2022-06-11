b999b0c547c60fb2289ee52df28490239e7fb05a
Emma Egstad <emma@egstad.com>
2022-06-11 13:04:30 -0500

    added svg logo assets

---
+++ public/assets/logo/logo-letter-e.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+export default function LogoLetterE() {
+    return (
+        <svg
+            width="108"
+            height="116"
+            viewBox="0 0 108 116"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <g styles="mix-blend-mode:overlay" filter="url(#filter0_i_84_3528)">
+                <path
+                    d="M58.4737 115.48C85.3837 115.48 105.111 105.249 105.111 92.0802C105.111 81.4438 98.434 75.7711 91.3525 75.7711C86.4965 75.7711 75.5706 76.9867 69.1972 76.9867C59.4853 76.9867 50.8863 75.8724 47.042 74.6569C44.8163 73.9478 42.9953 72.2257 42.9953 69.5919C42.9953 66.8569 45.221 64.7296 48.0536 64.7296C50.6839 64.7296 55.3375 66.8569 69.1972 66.8569C75.9753 66.8569 89.4303 66.3504 97.2201 65.6413C104.2 60.2725 107.539 54.5998 107.539 42.3427C107.539 18.5376 86.8 0 54.1236 0C20.3343 0 0 23.5012 0 57.3348C0 91.7763 21.1436 115.48 58.4737 115.48ZM48.7618 42.6466V30.2882C48.7618 27.1479 50.2793 24.8181 53.8201 24.8181C57.3608 24.8181 58.8783 27.1479 58.8783 30.2882V42.6466C58.8783 45.7868 57.3608 48.1167 53.8201 48.1167C50.2793 48.1167 48.7618 45.7868 48.7618 42.6466Z"
+                    fill="white"
+                    fillOpacity="0.3"
+                />
+            </g>
+            <defs>
+                <filter
+                    id="filter0_i_84_3528"
+                    x="0"
+                    y="0"
+                    width="107.539"
+                    height="115.48"
+                    filterUnits="userSpaceOnUse"
+                    colorInterpolationFilters="sRGB"
+                >
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                    <feBlend
+                        mode="normal"
+                        in="SourceGraphic"
+                        in2="BackgroundImageFix"
+                        result="shape"
+                    />
+                    <feColorMatrix
+                        in="SourceAlpha"
+                        type="matrix"
+                        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                        result="hardAlpha"
+                    />
+                    <feOffset />
+                    <feGaussianBlur stdDeviation="15.1088" />
+                    <feComposite
+                        in2="hardAlpha"
+                        operator="arithmetic"
+                        k2="-1"
+                        k3="1"
+                    />
+                    <feColorMatrix
+                        type="matrix"
+                        values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 1 0"
+                    />
+                    <feBlend
+                        mode="normal"
+                        in2="shape"
+                        result="effect1_innerShadow_84_3528"
+                    />
+                </filter>
+            </defs>
+        </svg>
+    );
+}
