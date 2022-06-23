bf78d94681b2178fc5f206238d0f37db613243c1
Emma Egstad <emma@egstad.com>
2022-06-23 10:44:07 -0500

    removing text opacity

--- pages/about.js
+++ pages/about.js
@@ -28,25 +28,27 @@ export default function About() {
             onAnimationComplete={onAnimationComplete}
         >
             <section className={styles.aboutBio}>
-                <p className={styles.aboutBioPrimary}>
-                    Hi, I am Emma Egstad. Full-stack web developer based in
-                    Tulsa, OK. Missing until dinner time chase the pig around
-                    the house. Meow. Under the bed climb a tree, wait for a
-                    fireman jump to fireman then scratch his face or show belly
-                    attack curtains head nudges.
-                </p>
-                <p className={styles.aboutBioSecondary}>
-                    Stare at owner accusingly then wink bury the poop bury it
-                    deep yet freak human out make funny noise mow mow mow mow
-                    mow mow success now attack human scratch at fleas.
-                </p>
-                <p className={styles.aboutBioSecondary}>
-                    Kitty kitty cat slap dog in face carefully drink from water
-                    glass and then spill it everywhere and proceed to lick the
-                    puddle playing with balls of wool or climb leg. Do not try
-                    to mix old food with new one to fool me! loved it, hated it,
-                    loved it, hated it.
-                </p>
+                <main>
+                    <p className={styles.aboutBioPrimary}>
+                        Hi, I am Emma Egstad. Full-stack web developer based in
+                        Tulsa, OK. Missing until dinner time chase the pig
+                        around the house. Meow. Under the bed climb a tree, wait
+                        for a fireman jump to fireman then scratch his face or
+                        show belly attack curtains head nudges.
+                    </p>
+                    <p className={styles.aboutBioSecondary}>
+                        Stare at owner accusingly then wink bury the poop bury
+                        it deep yet freak human out make funny noise mow mow mow
+                        mow mow mow success now attack human scratch at fleas.
+                    </p>
+                    <p className={styles.aboutBioSecondary}>
+                        Kitty kitty cat slap dog in face carefully drink from
+                        water glass and then spill it everywhere and proceed to
+                        lick the puddle playing with balls of wool or climb leg.
+                        Do not try to mix old food with new one to fool me!
+                        loved it, hated it, loved it, hated it.
+                    </p>
+                </main>
                 <ul className={styles.aboutBioLinks}>
                     <Link href="https://github.com/emmaegstad">
                         <a
